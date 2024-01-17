fd3db0bda1b8416afe32964f189c6e3abbff87ab
goldenCitrus <goldenpotatobull@outlook.com>
2024-01-17 12:18:19 -0700

    Completed Prep4

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random rng = new Random();
+        int number = rng.Next(1, 11);
+        int guessedNum;
+        
+        Console.WriteLine("Welcome to the random number guesser! Please guess a number between 1 and 10!");
+        do
+        {
+            Console.WriteLine("What is your guess?");
+            string guess = Console.ReadLine();
+            guessedNum = int.Parse(guess);
+            if (guessedNum != number)
+            {
+                Console.WriteLine("Wrong! Guess again!");
+            }
+        } while (guessedNum != number);
+
+        Console.WriteLine("Congratulations! You guessed the correct number! You win!");
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int parsedInput;
+        List<int> userInputs = new List<int>();
+        do
+        {
+            Console.WriteLine("Enter any number above 0 to add to a list, or enter 0 to close the program!");
+            string userInput = Console.ReadLine();
+            parsedInput = int.Parse(userInput);
+
+            if (parsedInput != 0)
+            {
+                userInputs.Add(parsedInput);
+            }
+
+        } while (parsedInput != 0);
+        
+        foreach (int entry in userInputs)
+        {
+            Console.WriteLine(entry);
+        }
+        Console.WriteLine($"The sum is: {userInputs.Sum()}");
+        Console.WriteLine($"The average is: {userInputs.Average()}");
+        Console.WriteLine($"The largest number is: {userInputs.Max()}");
     }
 }
