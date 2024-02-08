a5f3aa2019cc0b48cfad3fea9b7ecc1f3e6941a9
unknown <goldenpotatobull@outlook.com>
2024-02-07 23:18:52 -0700

    Finished Learning03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction oneOne = new Fraction();
+        Console.WriteLine(oneOne.GetFractionString());
+        Console.WriteLine(oneOne.GetDecimalValue());
+
+        Fraction fiveOne =new Fraction(5);
+        Console.WriteLine(fiveOne.GetFractionString());
+        Console.WriteLine(fiveOne.GetDecimalValue());
+
+        Fraction threeFour =new Fraction(3, 4);
+        Console.WriteLine(threeFour.GetFractionString());
+        Console.WriteLine(threeFour.GetDecimalValue());
+
+        Fraction oneThree =new Fraction(1, 3);
+        Console.WriteLine(oneThree.GetFractionString());
+        Console.WriteLine(oneThree.GetDecimalValue());
     }
 }
